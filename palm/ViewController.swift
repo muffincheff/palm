e11a44e0f38004cdf855f8f38cd4b96727f96808
kwon <recipe.muffin@gmail.com>
2022-10-05 21:32:16 +0900

    Initial Commit

---
+++ palm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  palm
+//
+//  Created by orca on 2022/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ palmUITests/palmUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  palmUITestsLaunchTests.swift
+//  palmUITests
+//
+//  Created by orca on 2022/10/05.
+//
+
+import XCTest
+
+final class palmUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
